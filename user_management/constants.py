465ff93a2796254c854a15e76d54c3137dc9f042
Khanh Nguyen <khanhqnguyen0209@gmail.com>
2020-03-29 18:18:01 +0700

    feat: Create command to add permission on user table

--- user_management/constants.py
+++ user_management/constants.py
@@ -5,12 +5,23 @@
 
 from enum import Enum
 
-from menu.models.category import CategoyPermission
-from menu.models.item import ItemPermission
-
+from user_management.models import UserPermission
 
 class UserGroups(Enum):
     """ Enum contains the user group for authorization in menu """
 
     ADMIN = 'admin_user'
     STANDARD = 'standard_user'
+
+
+USER_PERSMISSION_MAPPING = {
+    UserGroups.ADMIN.value: [
+        UserPermission.add.value,
+        UserPermission.change.value,
+        UserPermission.delete.value,
+        UserPermission.view.value,
+    ],
+    UserGroups.STANDARD.value: [
+        UserPermission.add.value,
+    ]
+}
