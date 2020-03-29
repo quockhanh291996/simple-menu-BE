b19d84098a04a50840c434541d1e89bc6599f213
Khanh Nguyen <khanhqnguyen0209@gmail.com>
2020-03-29 18:18:27 +0700

    feat: Adding app nam on the permission enums

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+"""
+    file: user_management/models
+    purpose: Use the User model of djang-auth app, but will define the permission enum on user table here
+"""
+from enum import Enum
 
-# Create your models here.
+class UserPermission(Enum):
+    """ Enum contains the permission on user model """
+
+    # Name of app cotain user model
+    app_name = 'auth'
+
+    add = app_name + '.add_user'
+    change = app_name + '.change_user'
+    delete = app_name + '.delete_user'
+    view = app_name + '.view_user'
